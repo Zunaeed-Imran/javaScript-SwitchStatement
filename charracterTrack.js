6ec7b3dcbd0935bbd7a398f22b93608a38d40164
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-05 16:44:00 +0600

    A character is a vowel or consonant in javaScript
    A character is a vowel or consonant in javaScript

---
+++ charracterTrack.js
@@ -0,0 +1,85 @@
+let v = prompt("Enter any English Alfabet.");
+
+switch (v) {
+  case "a":
+    document.write(v + " is Vowel.");
+    break;
+  case "b":
+    document.write(v + " is Consonant.");
+    break;
+  case "c":
+    document.write(v + " is Consonant");
+    break;
+  case "d":
+    document.write(v + " is Consonent.");
+    break;
+  case "e":
+    document.write(v + " is Vowel");
+    break;
+  case "f":
+    document.write(v + " is Consonent.");
+    break;
+  case "g":
+    document.write(v + " is Consonent.");
+    break;
+  case "h":
+    document.write(v + " is Consonent.");
+    break;
+  case "i":
+    document.write(v + " is Vowel.");
+    break;
+  case "j":
+    document.write(v + " is Consonent.");
+    break;
+  case "k":
+    document.write(v + " is Consonent.");
+    break;
+  case "l":
+    document.write(v + " is Consonent.");
+    break;
+   case "m":
+    document.write(v + " is Consonent.");
+    break;
+   case "n":
+    document.write(v + " is Consonent.");
+    break;
+   case "o":
+    document.write(v + " is Vowel.");
+    break;
+  case "p":
+    document.write(v + " is Consonent.");
+    break;
+  case "q":
+    document.write(v + " is Consonent.");
+    break;
+  case "r":
+    document.write(v + " is Consonent.");
+    break;
+  case "s":
+    document.write(v + " is Consonent.");
+    break;
+  case "t":
+    document.write(v + " is Consonent.");
+    break;
+  case "u":
+    document.write(v + " is Vowel.");
+    break;
+  case "v":
+    document.write(v + " is Consonent.");
+    break;
+  case "w":
+    document.write(v + " is Consonent.");
+    break;
+  case "x":
+    document.write(v + " is Consonent.");
+    break;
+  case "y":
+    document.write(v + " is Consonent.");
+    break;
+  case "z":
+    document.write(v + " is Consonent.");
+    break;
+  default:
+    document.write("Invalid input!.");
+}
+
