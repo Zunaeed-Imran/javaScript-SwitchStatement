859c8c9fcfc55c571658890f7a7ed92bc86bbf0a
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-05 17:47:08 +0600

    take multiple case in one break switch statement
    take multiple case in one break javascript switch statement.

---
+++ diffCase.js
@@ -0,0 +1,17 @@
+let animal = prompt("Enter animal Name you like")
+
+switch (animal) {
+  case "cat":
+  case "bobcat":
+  case "jaguar":  
+    document.write("cats are great")
+    break
+  case "dog":
+    document.write("Dog are kind of loud")
+    break
+  case "shark":
+    document.write("it's interesting choice")
+    break
+  default:
+    document.write("I naver hard that name Before")
+}
